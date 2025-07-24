59a0c2f913428ad1d16d7bfed32e6856482adbaf
Aaron BenDaniel <144371000+AaronBenDaniel@users.noreply.github.com>
2025-07-24 15:28:40 -0400

    correct download urls

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-curl "https://github.com/AaronBenDaniel/MaximilianGT500-wp-downloader-termux-install-script/blob/main/install.sh" -o install.sh
-curl "https://github.com/AaronBenDaniel/MaximilianGT500-wp-downloader-termux-install-script/blob/main/start.sh" -o start.sh
-curl "https://github.com/AaronBenDaniel/MaximilianGT500-wp-downloader-termux-install-script/blob/main/stop.sh" -o stop.sh
+curl "https://raw.githubusercontent.com/AaronBenDaniel/MaximilianGT500-wp-downloader-termux-install-script/refs/heads/main/install.sh" -o install.sh
+curl "https://raw.githubusercontent.com/AaronBenDaniel/MaximilianGT500-wp-downloader-termux-install-script/refs/heads/main/start.sh" -o start.sh
+curl "https://raw.githubusercontent.com/AaronBenDaniel/MaximilianGT500-wp-downloader-termux-install-script/refs/heads/main/stop.sh" -o stop.sh
 chmod +x install.sh
 chmod +x start.sh
 chmod +x stop.sh
