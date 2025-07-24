ddf49f2f12ca7c63b7b9a4d93a0f8c8750ebdead
Aaron BenDaniel <144371000+AaronBenDaniel@users.noreply.github.com>
2025-07-24 15:14:14 -0400

    mark scripts as executable

--- download.sh
+++ download.sh
@@ -2,3 +2,7 @@
 curl "https://github.com/AaronBenDaniel/MaximilianGT500-wp-downloader-termux-install-script/blob/main/install.sh" -o install.sh
 curl "https://github.com/AaronBenDaniel/MaximilianGT500-wp-downloader-termux-install-script/blob/main/start.sh" -o start.sh
 curl "https://github.com/AaronBenDaniel/MaximilianGT500-wp-downloader-termux-install-script/blob/main/stop.sh" -o stop.sh
+chmod +x install.sh
+chmod +x start.sh
+chmod +x stop.sh
+
