549dc0ed081c110fa5d9943ebf1294fe247f622a
Aaron BenDaniel <144371000+AaronBenDaniel@users.noreply.github.com>
2025-07-24 15:07:42 -0400

    added download script

---
+++ download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl "https://github.com/AaronBenDaniel/MaximilianGT500-wp-downloader-termux-install-script/blob/main/install.sh" -o install.sh
+curl "https://github.com/AaronBenDaniel/MaximilianGT500-wp-downloader-termux-install-script/blob/main/start.sh" -o start.sh
+curl "https://github.com/AaronBenDaniel/MaximilianGT500-wp-downloader-termux-install-script/blob/main/stop.sh" -o stop.sh
